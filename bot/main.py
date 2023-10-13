43d383072e9696c9d0c092e155711cc814af7e8e
anjanayraina <anjanayraina326@gmail.com>
2023-10-13 12:01:50 +0530

    Help added in bot

--- bot/main.py
+++ bot/main.py
@@ -18,3 +18,18 @@ def start(message):
     bot.reply_to(message,"Welcome to the MathWorks Interview Process " + user_first_name + " " + user_last_name +
                  " Hi I will be your buddy throughout this interview process.")
 
+
+@bot.message_handler(["help"])
+def help(message):
+    bot.reply_to(
+        message,
+        """
+        /start -> get started.
+        /help -> this particular message.
+        /facts -> get some facts about MathWorks.
+        /schedule -> get your interview timings.
+        /track -> view your selected track.
+        /interviewer -> get you interviewer's details.
+        /status -> get your interview status.
+        """,
+    )
