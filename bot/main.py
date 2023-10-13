c9268f85af75d4fb45c5506f1d3a4d02a6fa747c
anjanayraina <anjanayraina326@gmail.com>
2023-10-13 12:15:24 +0530

    Some facts added

--- bot/main.py
+++ bot/main.py
@@ -33,3 +33,20 @@ def help(message):
         /status -> get your interview status.
         """,
     )
+
+
+@bot.message_handler(["facts"])
+def facts(message):
+    fun_facts = [
+        "Founded: MathWorks was founded in 1984 by Cleve Moler, Jack Little, and Steve Bangert. The company is headquartered in Natick, Massachusetts, USA.",
+        "Flagship Product: MathWorks is best known for its flagship product, MATLAB. MATLAB is a high-level programming language and development environment that is widely used for numerical analysis, data visualization, and algorithm development.",
+        "Simulink: In addition to MATLAB, MathWorks also developed Simulink, a graphical programming environment for modeling, simulating, and analyzing dynamic systems. Simulink is widely used for designing and simulating control systems, signal processing algorithms, and more.",
+        "Industry Applications: MathWorks software solutions are used in various industries, including aerospace, automotive, communications, finance, medical devices, and robotics. MATLAB and Simulink are employed for research, development, and production purposes in these sectors.",
+        "Academic Adoption: MATLAB is widely used in academic institutions for teaching and research purposes. MathWorks offers academic licenses, resources, and support to educators, researchers, and students to facilitate learning and innovation in the field of mathematics and engineering.",
+        "Online Community: MathWorks maintains an active online community where users can access technical documentation, examples, and discussion forums to seek help, share knowledge, and collaborate with other MATLAB and Simulink users.",
+        "Toolboxes and Add-Ons: MathWorks offers a wide range of toolboxes and add-ons that extend the capabilities of MATLAB and Simulink. These toolboxes cover various domains, such as image processing, control systems, optimization, machine learning, and more.",
+        "Continuous Development: MathWorks continues to enhance its software offerings by regularly releasing updates and new versions. These updates introduce new features, performance improvements, and bug fixes to meet the evolving needs of its users.",
+        "Commitment to Education: MathWorks is committed to promoting STEM education and offers resources, competitions, and initiatives to encourage students interest in mathematics, engineering, and scientific computing.",
+    ]
+    random_index = random.randint(0, len(fun_facts) - 1)
+    bot.reply_to(message, fun_facts[random_index])
