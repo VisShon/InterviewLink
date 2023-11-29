79ac0de8255bf7eb9b9c558cb9ecd43961807ba6
anjanayraina <anjanayraina326@gmail.com>
2023-11-29 19:23:13 +0530

    start changed

--- bot/main.py
+++ bot/main.py
@@ -18,8 +18,8 @@ def start(message):
     user_id = str(message.from_user.id)
     user_first_name = str(message.from_user.first_name)
     user_last_name = str(message.from_user.last_name)
-    bot.reply_to(message,"Welcome to the MathWorks Interview Process " + user_first_name + " " + user_last_name +
-                 " Hi I will be your buddy throughout this interview process.")
+    bot.reply_to(message,"Welcome to the MathWorks Interview Process " + user_first_name + " " +
+                 "\nI will be your buddy throughout this interview process.")
     
 
 @bot.message_handler(["help"])
