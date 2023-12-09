e66761b96ff4847686a9a0086b537ecad4b737ad
Heemank <heemankv@gmail.com>
2023-12-10 00:15:05 +0530

    upload button

--- website/components/AdminUploadToolbar/OCRUpload.jsx
+++ website/components/AdminUploadToolbar/OCRUpload.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import UploadResumeButton from "../UploadResumeButton"
 
 
 function OCRUpload() {
@@ -8,19 +9,8 @@ function OCRUpload() {
 	}
 
 	return (
-		<button 
-			className="text-[0.75rem] hover:shadow-md  active:opacity-80 flex flex-col bg-secondary rounded-xl text-main justify-center items-center w-[8%] gap-2 h-[60%] "
-			id="Assessment"
-			onClick={uploadResumes}>
-			<Image
-				alt="assessment"
-				src={"/ocr.svg"}
-				height={20}
-				width={20}
-				className="w-[2rem] "
-			/>
-			<p>Upload Resume</p>
-		</button>
+		
+		<UploadResumeButton />
 	)
 }
 
